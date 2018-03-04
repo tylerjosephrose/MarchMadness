e13ea2e05b1e584ebc36c0aa1052cb4c6079fc1a
sethdippold21 <sethdippold21@gmail.com>
2018-03-03 20:13:36 -0500

    Display winner and score

--- MarchMadness.py
+++ MarchMadness.py
@@ -139,8 +139,13 @@ if __name__ == '__main__':
             secondTeamInput.configure(bg='white')
             # Send team values to the predictor to do analysis
             predictor = pred.Predictor()
-            winner = predictor.analyze(firstTeamInput.get(), secondTeamInput.get())
-            message = 'Expected Winner: \n' + winner
+            teamOneScore, teamTwoScore = predictor.analyze(firstTeamInput.get(), secondTeamInput.get())
+            if(teamOneScore > teamTwoScore):
+                message = 'Expected Winner: \n' + firstTeamInput.get() + '\nScore: ' + teamOneScore + ' - ' + teamTwoScore
+            elif(teamTwoScore > teamOneScore):
+                message = 'Expected Winner: \n' + secondTeamInput.get() + '\nScore: ' + teamTwoScore + ' - ' + teamOneScore
+            else:
+                message = 'Expected a tie. \n' + '\nScore: ' + teamTwoScore + ' - ' + teamOneScore
         else:
             message = 'Please enter a valid team.'
         winnerLabel.configure(text=message)
